f888f15f2191beb8c3f0074d7e8b665ca8685e83
zxw <wakakazw@163.com>
2018-12-20 12:38:57 +0800

    Initial commit

---
+++ app/src/main/java/com/zxw/flowlayout/MainActivity.java
@@ -0,0 +1,19 @@
+package com.zxw.flowlayout;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * Demo class
+ *
+ * @author zxw
+ * @date 2018/12/20
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
